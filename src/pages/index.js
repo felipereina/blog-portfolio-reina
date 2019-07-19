20cb0100ad4c495dfb119d3197a8a5bc4e16b7ac
Felipe Reina <41518538+felipereina@users.noreply.github.com>
2019-07-19 11:45:21 +0100

    created Blog Home Page - created Graphql communication

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,40 @@
 import React from "react"
 import Layout from "../components/layout"
+import PostList from "../components/PostList"
+import { graphql, useStaticQuery } from "gatsby"
 
-export default () => <Layout>Hello world!</Layout>
+const getPosts = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            title
+            author
+            slug
+            date(formatString: "MMMM Do, YYYY")
+            image {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+export default () => {
+  const response = useStaticQuery(getPosts)
+  const posts = response.allMdx.edges
+  return (
+    <Layout>
+      <PostList posts={posts} />
+    </Layout>
+  )
+}
